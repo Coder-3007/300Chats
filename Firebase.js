522ff5fbcce1a15f38763d3668773c6cff0b6393
Coder-3007 <coder3007@gmail.com>
2023-05-20 02:35:01 +0500

    chat app

---
+++ Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAScigkXPoNwLtSyQR7D8txtGb5gGMpAs",
+  authDomain: "chats-60c96.firebaseapp.com",
+  projectId: "chats-60c96",
+  storageBucket: "chats-60c96.appspot.com",
+  messagingSenderId: "928291101565",
+  appId: "1:928291101565:web:37232eea699e6d79f2ca14",
+  measurementId: "G-TDQDWNTTJ7",
+};
